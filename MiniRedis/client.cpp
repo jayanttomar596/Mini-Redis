b604d7c7eb132c0349268f2e63f93be2ced8f726
jayanttomar596 <jayanttomar596@gmail.com>
2026-03-30 19:51:41 +0530

    Implemented Multithreading to handle multiple clients at same time

--- MiniRedis/client.cpp
+++ MiniRedis/client.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
 int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (sock < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
     sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return 1;
+    }
+
+    cout << "Connected to server\n";
 
     char buffer[1024];
 
@@ -21,18 +36,32 @@ int main() {
         string cmd;
         getline(cin, cmd);
 
-        if (cmd == "EXIT") break;
+        if (cmd == "EXIT") {
+            close(sock);
+            break;
+        }
 
         string msg = cmd + "\n";
-        send(sock, msg.c_str(), msg.size(), 0);
+
+        if (send(sock, msg.c_str(), msg.size(), 0) < 0) {
+            perror("Send failed");
+            break;
+        }
 
         memset(buffer, 0, sizeof(buffer));
+
         int valread = recv(sock, buffer, 1024, 0);
 
-        cout.write(buffer, valread);
-        cout << endl;
+        if (valread <= 0) {
+            cout << "Server closed connection\n";
+            break;
+        }
+
+        cout << string(buffer, valread);
     }
 
-    close(sock);
+    return 0;
 }
+
+
 
--- MiniRedis/server/Server.cpp
+++ MiniRedis/server/Server.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sys/socket.h>
 #include <cstring>
+#include <thread>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "../utils/Logger.h"
@@ -35,8 +37,80 @@ void loadData(KVStore &kv) {
 
 
 
+
+
+
+void handleClient(int client_socket, KVStore &kv) {
+    char buffer[1024];
+    string pending = "";
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+
+        int valread = recv(client_socket, buffer, 1024, 0);
+
+        if (valread == 0) {
+            cout << "Client disconnected\n";
+            close(client_socket);
+            break;
+        }
+        if (valread < 0) {
+            perror("recv failed");
+            close(client_socket);
+            break;
+        }
+
+        pending += string(buffer, valread);
+
+        size_t pos;
+        while ((pos = pending.find('\n')) != string::npos) {
+
+            string line = pending.substr(0, pos);
+            pending.erase(0, pos + 1);
+
+            if (!line.empty() && line.back() == '\r') {
+                line.pop_back();
+            }
+
+            vector<string> tokens = CommandParser::parse(line);
+
+            if (tokens.empty()) continue;
+
+            string response;
+
+            if (tokens[0] == "SET" && tokens.size() == 3) {
+                kv.set(tokens[1], tokens[2]);
+                Logger::log("SET " + tokens[1] + " " + tokens[2]);
+                response = "OK\n";
+            }
+            else if (tokens[0] == "GET") {
+                response = kv.get(tokens[1]) + "\n";
+            }
+            else if (tokens[0] == "DEL") {
+                kv.del(tokens[1]);
+                Logger::log("DEL " + tokens[1]);
+                response = "Deleted\n";
+            }
+            else {
+                response = "Invalid Command\n";
+            }
+
+            send(client_socket, response.c_str(), response.size(), 0);
+        }
+    }
+}
+
+
+
+
+
+
+
 void Server::start(int port) {
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     if (server_fd < 0) {
         perror("Socket failed");
@@ -48,7 +122,8 @@ void Server::start(int port) {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) 
+    {
         perror("Bind failed");
         return;
     }
@@ -62,10 +137,13 @@ void Server::start(int port) {
 
     cout << "Server started on port " << port << endl;
 
-    int addrlen = sizeof(address);
+    // int addrlen = sizeof(address);
 
     while (true) {
-        int client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        sockaddr_in client_addr;
+        socklen_t addrlen = sizeof(client_addr);
+
+        int client_socket = accept(server_fd, (struct sockaddr*)&client_addr, &addrlen);
 
         if (client_socket < 0) {
             perror("Accept failed");
@@ -74,53 +152,23 @@ void Server::start(int port) {
 
         cout << "Client connected\n";
 
-        char buffer[1024];
-
-        while (true) {
-            memset(buffer, 0, sizeof(buffer));
-
-            int valread = read(client_socket, buffer, 1024);
-
-            if (valread <= 0) {
-                cout << "Client disconnected\n";
-                close(client_socket);
-                break;
-            }
-
-            string input(buffer, valread);
-
-            // remove newline characters
-            if (!input.empty() && input.back() == '\n') input.pop_back();
-            if (!input.empty() && input.back() == '\r') input.pop_back();
-
-            vector<string> tokens = CommandParser::parse(input);
-
-            if (tokens.empty()) continue;
-
-            string response;
-
-            if (tokens[0] == "SET" && tokens.size() == 3) {
-                kv.set(tokens[1], tokens[2]);
-
-                Logger::log("SET " + tokens[1] + " " + tokens[2]);
-
-                response = "OK\n";
-            }
-            else if (tokens[0] == "GET" && tokens.size() == 2) {
-                response = kv.get(tokens[1]) + "\n";
-            }
-            else if (tokens[0] == "DEL" && tokens.size() == 2) {
-                kv.del(tokens[1]);
-
-                Logger::log("DEL " + tokens[1]);
-
-                response = "Deleted\n";
-            }
-            else {
-                response = "Invalid Command\n";
-            }
-
-            send(client_socket, response.c_str(), response.size(), 0);
-        }
+        // create thread
+        thread t([this, client_socket]() {
+            handleClient(client_socket, this->kv);
+        });
+        t.detach();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
