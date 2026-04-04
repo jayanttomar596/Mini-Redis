cce40ab10c683de0c92861a09d6bed4a87610c55
jayanttomar596 <jayanttomar596@gmail.com>
2026-04-04 12:39:20 +0530

    used mutex locks to ensure thread-safe access to the shared key-value store, preventing race conditions during concurrent operations

--- MiniRedis/store/KVStore.cpp
+++ MiniRedis/store/KVStore.cpp
@@ -1,15 +1,28 @@
 #include "KVStore.h"
 
+
 void KVStore::set(const std::string &key, const std::string &value) {
+    std::lock_guard<std::mutex> lock(mtx);   // LOCK
     store[key] = value;
 }
 
+
 std::string KVStore::get(const std::string &key) {
+    std::lock_guard<std::mutex> lock(mtx);  // LOCK
+
     if (store.find(key) != store.end())
         return store[key];
+
     return "NULL";
 }
 
+
+
 void KVStore::del(const std::string &key) {
+    std::lock_guard<std::mutex> lock(mtx);  // LOCK
     store.erase(key);
 }
+
+
+
+
--- MiniRedis/store/KVStore.h
+++ MiniRedis/store/KVStore.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <unordered_map>
+#include <mutex>
 #include <string>
+
+
+
 
 class KVStore {
 private:
     std::unordered_map<std::string, std::string> store;
+    std::mutex mtx;   
 
 public:
     void set(const std::string &key, const std::string &value);
     std::string get(const std::string &key);
     void del(const std::string &key);
 };
+
+
+
+
