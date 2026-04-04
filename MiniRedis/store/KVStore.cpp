97b05ad453ec7097f5641817b5c226a85c1db2ce
jayanttomar596 <jayanttomar596@gmail.com>
2026-04-04 13:21:15 +0530

    implemented an LRU eviction policy using a hash map and doubly linked list to ensure O(1) access and eviction

--- MiniRedis/store/KVStore.cpp
+++ MiniRedis/store/KVStore.cpp
@@ -2,25 +2,53 @@
 
 
 void KVStore::set(const std::string &key, const std::string &value) {
-    std::lock_guard<std::mutex> lock(mtx);   // LOCK
-    store[key] = value;
+    std::lock_guard<std::mutex> lock(mtx);
+
+    // If key exists → update + move to front
+    if (store.find(key) != store.end()) {
+        lru.erase(store[key].second);
+    }
+    else if (store.size() >= capacity) {
+        // remove least recently used (back)
+        std::string lru_key = lru.back();
+        lru.pop_back();
+        store.erase(lru_key);
+    }
+
+    // insert at front
+    lru.push_front(key);
+    store[key] = {value, lru.begin()};
 }
+
+
+
 
 
 std::string KVStore::get(const std::string &key) {
-    std::lock_guard<std::mutex> lock(mtx);  // LOCK
+    std::lock_guard<std::mutex> lock(mtx);
 
-    if (store.find(key) != store.end())
-        return store[key];
+    if (store.find(key) == store.end()) {
+        return "NULL";
+    }
 
-    return "NULL";
+    // move to front (recently used)
+    lru.erase(store[key].second);
+    lru.push_front(key);
+    store[key].second = lru.begin();
+
+    return store[key].first;
 }
+
 
 
 
 void KVStore::del(const std::string &key) {
-    std::lock_guard<std::mutex> lock(mtx);  // LOCK
-    store.erase(key);
+    std::lock_guard<std::mutex> lock(mtx);
+
+    if (store.find(key) != store.end()) {
+        lru.erase(store[key].second);
+        store.erase(key);
+    }
 }
 
 
--- MiniRedis/store/KVStore.h
+++ MiniRedis/store/KVStore.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <unordered_map>
 #include <mutex>
+#include <list>
 #include <string>
 
 
@@ -8,8 +9,15 @@
 
 class KVStore {
 private:
-    std::unordered_map<std::string, std::string> store;
-    std::mutex mtx;   
+    int capacity = 5;  //small for testing
+
+    // key → (value, iterator in list)
+    std::unordered_map<std::string, std::pair<std::string, std::list<std::string>::iterator>> store;
+
+    // doubly linked list (most recent at front)
+    std::list<std::string> lru;
+
+    std::mutex mtx;
 
 public:
     void set(const std::string &key, const std::string &value);
